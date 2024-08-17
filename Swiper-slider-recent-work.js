5dc5b5275b1657215ae7e85f07d55fa1f09c5e3c
Masud Rana <masudrana730@gmail.com>
2024-08-17 16:47:32 +0600

    recent work part update

---
+++ Swiper-slider-recent-work.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+      },
+      
+  });
