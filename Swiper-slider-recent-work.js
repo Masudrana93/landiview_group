a0d835a67608905c9be55694f821d5d2df1d635c
Masud Rana <masudrana730@gmail.com>
2024-09-07 17:45:18 +0600

    swiper slider responsive

--- Swiper-slider-recent-work.js
+++ Swiper-slider-recent-work.js
@@ -1,16 +1,29 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
+    loop: true ,
     spaceBetween: 30,
     centeredSlides: true,
     grabCursor: true,
+    responsiveClass:true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    autoplay: {
-        delay: 3000,
-      },
-      
+    // autoplay: {
+    //     delay: 3000,
+    //   },
+
+      breakpoints:{
+        0:{
+          slidesPerView:1,            
+        },
+        600:{
+          slidesPerView:3,            
+        },
+        1000:{
+          slidesPerView:3,           
+        }
+    }     
       
   });
 
